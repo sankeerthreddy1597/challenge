e25e5090906dae13a61a5741b425fbd3d05b8976
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2021-08-12 14:32:58 -0400

    get user/age/:item endpoint complete

--- src/database/index.js
+++ src/database/index.js
@@ -1,34 +1,79 @@
-'use strict';
-const _ = require('lodash');
-const db = require('./db.js');
-
+"use strict";
+const _ = require("lodash");
+const db = require("./db.js");
 
 // UTILS
 //----------------
 // This is a mock db call that waits for # milliseconds and returns
 const mockDBCall = (dataAccessMethod) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(dataAccessMethod());
-        }, 500);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(dataAccessMethod());
+    }, 500);
+  });
 };
 
 // MOCK DB CALLS
 //----------------
 const getUsers = () => {
-    const dataAccessMethod = () => _.map(db.usersById, userInfo => userInfo)
-    return mockDBCall(dataAccessMethod);
+  const dataAccessMethod = () => _.map(db.usersById, (userInfo) => userInfo);
+  return mockDBCall(dataAccessMethod);
 };
 
 const getListOfAgesOfUsersWith = (item) => {
-    const dataAccessMethod = () => {
-        // fill me in :)
+  const dataAccessMethod = () => {
+    // fill me in :)
+    try {
+      //Array to store users with item
+      let users = [];
+
+      //Store the users who have given item in an array
+      for (const user in db.itemsOfUserByUsername) {
+        db.itemsOfUserByUsername[user].forEach((h) => {
+          if (h === item) {
+            users.push(user);
+          }
+        });
+      }
+
+      //Handling No users with item
+      if (users.length === 0) {
+        return [];
+      }
+
+      //Get age and count for each user retrieved
+      let itemUserAgeCount = {};
+      users.forEach((user) => {
+        for (const id in db.usersById) {
+          if (db.usersById[id].username === user) {
+            if (db.usersById[id].age in itemUserAgeCount) {
+              itemUserAgeCount[db.usersById[id].age]++;
+            } else {
+              itemUserAgeCount[db.usersById[id].age] = 1;
+            }
+          }
+        }
+      });
+
+      //return array of objects with age and count
+      let res = [];
+
+      for (const userAge in itemUserAgeCount) {
+        res.push({ age: parseInt(userAge), count: itemUserAgeCount[userAge] });
+      }
+      return res;
+    } catch (error) {
+      //Return False and error message instead of crashing server.
+      return {
+        success: false,
+        message: error.message,
+      };
     }
-    return mockDBCall(dataAccessMethod);
-}
+  };
+  return mockDBCall(dataAccessMethod);
+};
 
 module.exports = {
-    getUsers,
-    getListOfAgesOfUsersWith
+  getUsers,
+  getListOfAgesOfUsersWith,
 };
--- src/routes/getListOfAgesOfUsersWith.js
+++ src/routes/getListOfAgesOfUsersWith.js
@@ -1,12 +1,12 @@
-'use strict';
-const mockDBCalls = require('../database/index.js');
+"use strict";
+const mockDBCalls = require("../database/index.js");
 
 const getListOfAgesOfUsersWithHandler = async (request, response) => {
-    const itemToLookup = 'carrot';
-    const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
-    return response.status(200).send(JSON.stringify(data));
+  const itemToLookup = request.params.item;
+  const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
+  return response.status(200).send(JSON.stringify(data));
 };
 
 module.exports = (app) => {
-    app.get('/users/age', getListOfAgesOfUsersWithHandler);
+  app.get("/users/age/:item", getListOfAgesOfUsersWithHandler);
 };
